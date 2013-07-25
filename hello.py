96f50b154c4d796a80d1c344e31b9507f7c414af
irma <your_email@whatever.com>
2013-07-25 21:01:44 +0300

    print command arguments

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
 import datetime
+import sys
 
 print "Hello world! Current time is %s" % datetime.datetime.strftime(datetime.datetime.now(), format="%d.%m.%Y") 
+print "agruments are %s" % ", ".join(sys.argv[1:])
